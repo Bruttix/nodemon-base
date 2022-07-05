58026af189281368b293e32b18575626a698f484
Bruttix <bgreene212@gmail.com>
2022-07-05 11:59:45 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+app.listen(8080, ()=>{
+	console.log('running on port 8080');
+});
+app.get('/', (req, res)=>{
+	res.send('<h1>Meta World Found</h1>');
+});
